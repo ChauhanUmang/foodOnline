4473f42a9084c91795b9174f0ce0d7241ff3652a
Umang Chauhan <umang4191@outlook.com>
2023-07-24 22:29:12 +0200

    Setup: register customer page

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from accounts.forms import UserForm
+from accounts.models import User
 
 
 # Create your views here.
 def register_user(request):
-    return HttpResponse("This is a user registration page.")
+    if request.method == "POST":
+        print(request.POST)
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.role = User.CUSTOMER
+            user.save()
+            return redirect('register_user')
+    else:
+        form = UserForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'accounts/registerUser.html', context)

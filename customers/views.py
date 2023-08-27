b1355daa82bb781530ed0a30ab4ddb78decc290f
Umang Chauhan <umang4191@outlook.com>
2023-08-27 17:39:40 +0200

    Created user profile form

--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+from accounts.models import UserProfile
+from accounts.views import check_role_customer
+from accounts.forms import UserProfileForm, UserInfoForm
 
 
-# Create your views here.
+@login_required(login_url='login')
+@user_passes_test(check_role_customer)
 def profile(request):
-    return render(request, 'customers/profile.html')
+    existing_profile = get_object_or_404(UserProfile, user=request.user)
+    profile_form = UserProfileForm(instance=existing_profile)
+    user_form = UserInfoForm(instance=request.user)
+
+    context = {
+        'profile_form': profile_form,
+        'user_form': user_form
+    }
+
+    return render(request, 'customers/profile.html', context)
 
